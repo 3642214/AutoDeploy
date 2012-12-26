a77086fe1238af0ead1e7d82179610ad66c5a4bb
zj <jue.zheng@csst.com>
2012-12-26 17:29:04 +0800

    修改

--- basic/configSoft.sh
+++ basic/configSoft.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 INSTALL="/home/testuser/workspace"
 unzipPath="/home/testuser/workspace/tmp/"
-[ -z "$1" ] || { echo "configSoft parameter error";exit -1; }
+[ -n "$1" ] || { echo "configSoft parameter error";exit -1; }
 unzipFile.sh $1
 cd $unzipPath
 mv * ../
--- basic/unzipFile.sh
+++ basic/unzipFile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 unzipPath="/home/testuser/workspace/tmp/"
-[ -z "$1" ] || { echo "unzip parameter error";exit -1; }
+[ -n "$1" ] || { echo "unzip parameter error";exit -1; }
 		cd git/Final/$1/
 		unzip -q *.zip -d $unzipPath
