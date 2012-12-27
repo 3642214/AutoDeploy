e8d850ee61679c9562ff19ec38a4fbf6405c64ac
jue.zheng <jue.zheng@csst.com>
2012-12-27 17:00:30 +0800

    modify readState.sh

--- basic/readState.sh
+++ basic/readState.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 stateFileName=/home/testuser/State
 [ -n "$1" ] || { echo "readConfig parameter error:no parameter";exit -1; }
-	value=`grep $1 $stateFileName` || exit $?
+	value=`grep "^$1=" $stateFileName` || exit $?
 		 echo $value | cut -d"=" -f 2 
