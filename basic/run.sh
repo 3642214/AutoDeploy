46ec712208166d639add18422bef4ec064fc9f8d
zj <jue.zheng@csst.com>
2012-12-31 16:52:24 +0800

    修改run

--- basic/run.sh
+++ basic/run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-softPath=/home/testuser/workspace
-softName=
+. conf_path
 [ $# = 1 ] || { "run parameter error";exit -1; } 
-		softName=`nameToPath.sh $1`
+		softName=`bash nameToPath.sh $1` || exit $?
 		cd $softPath
-		if [ $1 = "ZK" or $1 = "zk"]
+		if [ $1 = "ZK" or $1 = "zk" ]
 			then		
-				screen -S ZK -dm ./$softName/bin/zkServer.sh start
+				screen -S ZK -dm ./$installPath/bin/zkServer.sh start
 			else
-				screen -S $softName -dm ./$softName/bin/$softName console
+				screen -S $softName -dm ./$installPath/bin/$softName console
 				[ $? = "0" ] || { echo "run $softName error";exit -1; }
-				 		writeLog.sh $softName
+				 	writeLog.sh $softName || exit $?
 		fi
