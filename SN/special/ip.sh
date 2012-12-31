d531e94adb1b4f77697c4f1be4a3354f38b00bea
zj <jue.zheng@csst.com>
2012-12-31 10:14:59 +0800

    1.xiugai

--- SN/special/ip.sh
+++ SN/special/ip.sh
@@ -2,4 +2,4 @@
 vmFile=/home/testuser/workspace/skyFs-storage/etc/vm.args
 [ -f $vmFile ] || echo "$vmFile can not Found";exit -1
 	LOCALHOST_IP="$(ip -f inet addr | grep global | awk '{print $2}' | awk -F/ '{print $1}')"
-	sed -i "s/-name.*/-name skyFs-storage@$LOCALHOST_IP/g" $vmFile
+	sed -i "s/-name.*/-name SN@$LOCALHOST_IP/g" $vmFile
