7e9f75d16d49942a53c22bf6c4a78858fb3f298c
zj <jue.zheng@csst.com>
2012-12-31 17:37:54 +0800

    修改initState

--- basic/initState.sh
+++ basic/initState.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #clear the config file
-[ -f $stateFileName ] || touch $stateFileName
+. conf_path
+[ -f $stateFileName ] || touch $stateFileName || { echo "StateFile error";exit -4; }
 echo "soft=" >> $stateFileName
 echo "git=" >> $stateFileName
 echo "date=" >> $stateFileName
