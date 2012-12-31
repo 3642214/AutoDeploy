f9d52443fcf5573ba363eb75760af7b334a0a43b
zj <jue.zheng@csst.com>
2012-12-31 15:48:20 +0800

    1xiugai

--- basic/configSoft.sh
+++ basic/configSoft.sh
@@ -3,12 +3,12 @@
 [ -d $unzipPath ] || mkdir -p $unzipPath
 bash unzipFile.sh $1 || exit $?
 cd $unzipPath
-mv * ../
+mv -f * ../
 rm -fr $unzipPath
 softName=`bash nameToPath.sh $1` || exit $?
 #copy default config file
 rm -fr $installPath/$softName/etc/*
-cp -r git/$1/config/default/* $installPath/$softName/etc/
+cp -r /root/git/$1/config/default/* $installPath/$softName/etc/
 #special config
 for shell in `ls -1 git/$1/config/special/`
 	{ 
