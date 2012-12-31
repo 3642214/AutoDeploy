9785ccbc469cb5853ac9bfc97d99a2fb11f26e41
zj <jue.zheng@csst.com>
2012-12-31 16:54:16 +0800

    1

--- basic/run.sh
+++ basic/run.sh
@@ -3,11 +3,11 @@
 [ $# = 1 ] || { "run parameter error";exit -1; } 
 		softName=`bash nameToPath.sh $1` || exit $?
 		cd $softPath
-		if [ $1 = "ZK" or $1 = "zk" ]
+		if [ $1 = "ZK" -a $1 = "zk" ]
 			then		
 				screen -S ZK -dm ./$installPath/bin/zkServer.sh start
 			else
 				screen -S $softName -dm ./$installPath/bin/$softName console
 				[ $? = "0" ] || { echo "run $softName error";exit -1; }
-				 	writeLog.sh $softName || exit $?
+				 	writeState.sh $softName || exit $?
 		fi
