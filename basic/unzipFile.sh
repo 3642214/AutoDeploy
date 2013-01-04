ac4e3dbd6a747bf62966d34e60bd27bc5b4398a9
zj <jue.zheng@csst.com>
2013-01-04 16:55:03 +0800

    修改

--- basic/unzipFile.sh
+++ basic/unzipFile.sh
@@ -2,7 +2,7 @@
 . conf_path
 [ -n "$1" ] || { echo "unzip parameter error:no parameter";exit -10; }
 	cd /root/git/$1/
-	if [ $1 = "zk" -a $1 = "ZK" ]
+	if [ $1 = "zk" -o $1 = "ZK" ]
 		then
 			tar zxf zookeeper-3.3.3.tar.gz -C $unzipPath || { echo "tar error";exit -11; }
 		else 
