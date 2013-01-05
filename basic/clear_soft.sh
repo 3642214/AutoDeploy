9e3e3f1e483a00758f29ce5742cc2339c8505fd2
zj <jue.zheng@csst.com>
2013-01-05 16:16:42 +0800

    modify

--- basic/clear_soft.sh
+++ basic/clear_soft.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 . conf_path
 PATH=$PATH:/root/git/shell/basic
-[ -n "$1" ] || { echo "clear_soft parameter error:no parameter";exit -20; }
-softName=`bash nameToPath.sh $1` || exit $?
-rm -fr $installPath/$softName $stateFileName
+	[ -f $stateFileName ] || { echo "not found stateFile";exit -20; };
+	job=`bash readState.sh soft` || { echo "readState error";exit -21; }
+	softName=`bash nameToPath.sh $1` || exit $?
+	rm -fr $installPath/$softName $stateFileName
