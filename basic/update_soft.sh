077c9a51dfef598af54b88665950592c88225cc2
zj <jue.zheng@csst.com>
2013-01-05 15:24:27 +0800

    12

--- basic/update_soft.sh
+++ basic/update_soft.sh
@@ -4,12 +4,12 @@ PATH=$PATH:/root/git/shell/basic
 [ -n "$1" ] || { echo "update_soft parameter error:no parameter";exit -15; }
 softName=`bash nameToPath.sh $1` || exit $?
 [ -d $installPath/$softName/lib ] || { echo "not found lib file";exit -16; }
-		cd $installPath/$softName/
-		#delete old path
-		rm -fr lib releases erts-*
 		[ -d $unzipPath ] || mkdir -p $unzipPath
 		bash unzipFile.sh $1 || exit $?
 		cd $unzipPath
+		#delete old path
+		cd $installPath/$softName/
+		rm -fr lib releases erts-*
 		mv -f skyFs-controller/lib $installPath/$softName/
 		mv -f skyFs-controller/releases $installPath/$softName/
 		mv -f skyFs-controller/erts-* $installPath/$softName/
