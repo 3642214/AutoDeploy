eee9163ef485794c75becc407e79595793d38582
zj <jue.zheng@csst.com>
2013-01-05 10:40:30 +0800

    修改

--- basic/run.sh
+++ basic/run.sh
@@ -3,12 +3,12 @@
 PATH=$PATH:/root/git/shell/basic
 [ $# = 1 ] || { "run parameter error: no parameter";exit -8; } 
 		softName=`bash nameToPath.sh $1` || exit $?
-		cd $softPath
-		if [ $1 = "ZK" -a $1 = "zk" ]
-			then		
-				screen -S ZK -dm ./$installPath/$softName/bin/zkServer.sh start
+		cd $installPath/$softName/bin/
+		if [ $1 = "ZK" -o $1 = "zk" ]
+			then			
+				screen -S ZK -dm ./zkServer.sh start
 			else
-				screen -S $softName -dm ./$installPath/$softName/bin/$softName console
+				screen -S $softName -dm ./$softName console
 				[ $? = "0" ] || { echo "run $softName error";exit -9; }
 				 	bash writeState.sh $softName || exit $?
 		fi
