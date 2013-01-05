972b1072183718d2be90e9a414e1b6a3a996ac6e
zj <jue.zheng@csst.com>
2013-01-05 11:23:32 +0800

    修改

--- basic/git_pull.sh
+++ basic/git_pull.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+. conf_path
 [ -n "$1" ] || { echo "git_pull parameter error:no parameter";exit -1; }
 softName=`bash nameToPath.sh $1` || exit $?
-[ -d /root/git/$1/.git ] || { mkdir -p /root/git;cd /root/git;git clone root@192.168.8.209:/root/git/final/$1; }
-	cd /root/git/$1
-	git pull || exit -2
+[ -d $gitPath/$1/.git ] || { mkdir -p $gitPath;cd $gitPath;git clone $gitServerAddr/$1; }
+	cd $gitPath/$1
+	git pull || { echo "git_pull error";exit -2; }
--- basic/writeState.sh
+++ basic/writeState.sh
@@ -3,6 +3,7 @@
 [ -n "$1" ] || { echo "writeState parameter error:no parameter";exit -17; }
 [ -f $stateFileName ] || bash initState.sh 
 		sed -i 's/^soft=.*$/soft='$1'/g' $stateFileName
+		cd $gitPath/$1
 		gitVersion=`git log|grep commit|sed -n 1p|cut -d" " -f 2`
 		sed -i "s/^git=.*$/git=$gitVersion/g" $stateFileName 
 		date=`date`
