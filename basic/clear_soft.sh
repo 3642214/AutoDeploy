5ed021ac99da9eced75827575b5dc129661892d3
zj <jue.zheng@csst.com>
2013-01-05 16:35:34 +0800

    d

--- basic/clear_soft.sh
+++ basic/clear_soft.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 . conf_path
 PATH=$PATH:/root/git/shell/basic
+#kill process
+! pgrep beam.smp || killall beam.smp
+#delete file and Statefile
 	[ -f $stateFileName ] || { echo "not found stateFile";exit -20; };
 	job=`bash readState.sh soft` || { echo "readState error";exit -21; }
 	softName=`bash nameToPath.sh $1` || exit $?
