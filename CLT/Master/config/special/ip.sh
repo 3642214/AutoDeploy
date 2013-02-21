0e1fb840d8900163119009b3be6e0a62816be368
zj <jue.zheng@csst.com>
2013-02-21 09:39:59 +0800

    12

--- CLT/Master/config/special/ip.sh
+++ CLT/Master/config/special/ip.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 vmFile=/home/testuser/workspace/skyFS-mapreduce/etc/vm.args
 [ -f $vmFile ] || { echo "$vmFile can not Found";exit -1; }
 	LOCALHOST_IP="$(ip -f inet addr | grep global | awk '{print $2}' | awk -F/ '{print $1}')"
