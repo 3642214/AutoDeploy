7178aa534de5fad68976a8fe478d68623d4ea753
zj <jue.zheng@csst.com>
2013-02-28 15:55:43 +0800

    1.增加通过hostname查找任务时，如果KV表没有该name的任务，则返回233错误。

--- basic/analyzeJob.sh
+++ basic/analyzeJob.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 . conf_path
 [ -n "$1" ] || { log "analyzeJob parameter error:no parameter";exit 253; }
- 		job=`grep $1 $jobTable` || exit $?
+ 	job=`grep $1 $jobTable` || {log "$1 not found JOB" ;exit 233; }
  		echo $job | cut -d"=" -f 2 | sed 's/\// /g'
