f34b1a879a3e2e27b602fd465c8f7b1ba8207f24
zj <jue.zheng@csst.com>
2013-03-12 17:44:48 +0800

    1.新增restart方法

---
+++ basic/restartSoft.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+. conf_path
+[ -n "$1" ] || { log "restartSoft parameter error:no parameter";exit 227; }
+softName=`bash nameToPath.sh $1` || exit $?
+	#kill process
+	pkill -f $softName/erts.*/bin/beam.smp
+bash run.sh $1
