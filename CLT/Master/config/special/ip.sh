80f4425025307a34fca75bb2a10c69a0de6abca8
zj <jue.zheng@csst.com>
2013-03-25 15:10:18 +0800

    1.修改bug

--- CLT/Master/config/special/ip.sh
+++ CLT/Master/config/special/ip.sh
@@ -3,7 +3,7 @@ MRPath=/home/testuser/workspace/skyFS-mapreduce
 [ -d $MRPath/releases ] || { echo "not found releases folder" ; exit 190 ; }
 floder=`ls -l $MRPath/releases | grep ^d | awk '{print $9}' | sort -nr | sed -n 1p`
 vmFile=$MRPath/releases/$floder/vm.args
-[ -n $1 ] || vmFile=$MRPath/etc/vm.args
+[ -z $1 ] || vmFile=$MRPath/etc/vm.args
 [ -f $vmFile ] || { echo "$vmFile can not Found";exit 191; }
         LOCALHOST_IP="$(ip -f inet addr | grep global | awk '{print $2}' | awk -F/ '{print $1}')"
         sed -i "s/-name.*/-name MR-M@$LOCALHOST_IP/g" $vmFile
