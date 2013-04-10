28c6a1b6da9879ccbba1d0334ebec97d28f038dd
zj <jue.zheng@csst.com>
2013-04-10 10:54:54 +0800

    2

--- basic/unzipFile.sh
+++ basic/unzipFile.sh
@@ -6,7 +6,7 @@
 	cd /root/git/$folder
 	if [ $1 = "zk" -o $1 = "ZK" ]
 		then
-			tar zxf zookeeper-3.3.3.tar.gz -C $unzipPath || { log "tar error";exit 245; }
+			tar zxf zookeeper-*.tar.gz -C $unzipPath || { log "tar error";exit 245; }
 		else 
 			unzip -q *.zip -d $unzipPath || { log "unzip error";exit 244; }
 	fi
