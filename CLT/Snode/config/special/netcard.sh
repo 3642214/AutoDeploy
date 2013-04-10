b861198ec84668c46ed53c5ea8ba17312a45ff08
zj <jue.zheng@csst.com>
2013-04-10 16:50:05 +0800

    缺逗号

--- CLT/Snode/config/special/netcard.sh
+++ CLT/Snode/config/special/netcard.sh
@@ -6,4 +6,4 @@ appFile=$MRPath/releases/$floder/sys.config
 [ -z $1 ] || appFile=$MRPath/etc/app.config
 [ -f $vmFile ] || { echo "$vmFile can not Found";exit 191; }
         netcard=`ifconfig |grep bond |wc -l`
-        [ $netcard -eq 0 ] || sed -i "s/{network_card,[0-9]*},/{network_card,4}/g" $appFile
+        [ $netcard -eq 0 ] || sed -i "s/{network_card,[0-9]*},/{network_card,4},/g" $appFile
