2da0ce8fbd591621b1a46d168c28e8fd76b60599
zj <jue.zheng@csst.com>
2013-04-18 11:00:58 +0800

    1.KV表结构更改，SN行最前使用关键字"<"用于指定SN的rack

--- SN/config/special/rack.sh
+++ SN/config/special/rack.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 SNPath=/home/testuser/workspace/skyFs-storage
+Name=`hostname | cut -d"." -f 1`
+KVFile=/root/git/shell/KV
+Rack=`grep $Name $KVFile | cut -d"<" -f 1` || { echo "not found KVFile" ; exit 193 ; }
 [ -d $SNPath/releases ] || { echo "not found releases folder" ; exit 190 ; }
 floder=`ls -l $SNPath/releases | grep ^d | awk '{print $9}' | sort -nr | sed -n 1p`
 appFile=$SNPath/releases/$floder/sys.config
 [ -f $appFile ] || { echo "$appFile not Found";exit 192; }
-	rack=`ip -f inet addr | grep global | awk '{print $2}' | awk -F/ '{print $1}'|cut -d'.' -f4`
-	sed -i 's/{path,.*}/{path,\"\/root\/rack\/storage'$rack'\"}/g' $appFile
+	Ip=`ip -f inet addr | grep global | awk '{print $2}' | awk -F/ '{print $1}'|cut -d'.' -f4`
+	sed -i 's/{path,.*}/{path,\"\/root\/'$Rack'\/storage'$Ip'\"}/g' $appFile
