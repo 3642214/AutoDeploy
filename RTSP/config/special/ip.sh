d9e360206dd776aa257f7ead9fc8459c65436bd2
zj <jue.zheng@csst.com>
2013-05-03 15:14:27 +0800

    bug

--- RTSP/config/special/ip.sh
+++ RTSP/config/special/ip.sh
@@ -3,7 +3,7 @@ RTSPPath=/home/testuser/workspace/RTSP-server
 [ -d $RTSPPath/releases ] || { echo "not found releases folder" ; exit 190 ; }
 floder=`ls -l $RTSPPath/releases | grep ^d | awk '{print $9}' | sort -nr | sed -n 1p`
 vmFile=$RTSPPath/releases/$floder/vm.args
-[ -z $1 ] || vmFile=$MRPath/etc/vm.args
+[ -z $1 ] || vmFile=$RTSPPath/etc/vm.args
 [ -f $vmFile ] || { echo "$vmFile can not Found";exit 191; }
         LOCALHOST_IP="$(ip -f inet addr | grep global | awk '{print $2}' | awk -F/ '{print $1}')"
         sed -i "s/-name.*/-name MR-S@$LOCALHOST_IP/g" $vmFile
