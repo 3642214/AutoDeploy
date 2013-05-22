ab5b08036cf4a9c367c4f94a8a7fe130579c551b
zj <jue.zheng@csst.com>
2013-05-22 17:30:52 +0800

    bug

--- basic/writeState.sh
+++ basic/writeState.sh
@@ -2,7 +2,7 @@
 PATH=$PATH:/root/git/shell/basic
 . conf_path
 [ -n "$1" ] || { log "writeState parameter error:no parameter";exit 239; }
-	[ grep "\[$1\]" $stateFileName ] || bash initStateValue.sh $1 || exit $?
+	grep "\[$1\]" $stateFileName || bash initStateValue.sh $1 || exit $?
 		#CLT_Master to CLT
 		floder=`echo $1 | cut -d"_" -f 1`
 		cd $gitPath/$floder
