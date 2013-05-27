6816b7899e99826d3907c76833b18bd92052309b
zj <jue.zheng@csst.com>
2013-05-27 10:52:27 +0800

    1.增加initWorkspace脚本，在大于200G硬盘分区中创建workspace文件夹，并link到root文件夹下

--- basic/initState.sh
+++ basic/initState.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#clear the config file
 PATH=$PATH:/root/git/shell/basic
 . conf_path
 [ -f $stateFileName ] || touch $stateFileName || { log "StateFile error";exit 252; }
---
+++ basic/initWorkspace.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+PATH=$PATH:/root/git/shell/basic
+. conf_path
+function init()
+  {
+  rm -fr $installPath
+  devCount=`df |wc -l`
+	for((i=2;i<=$devCount;i++))
+	do
+	  devSize=`df|awk '{print $2}'|sed -n "${i}p"`
+	  SIZE=$(( 200 * 1024 * 1024 ))
+	  if [ $devSize -gt $SIZE ]
+	    then
+	      volPath=`df|awk '{print $6}'|sed -n "${i}p"`
+	      break
+	  fi
+	done
+	mkdir -p $volPath/workspace
+	ls -s $volPath/workspace $installPath
+}
+[ -L $installPath ] || init || { log "mkdir workspace error";exit 225; }
