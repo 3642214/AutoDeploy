5a5d8a4a38dc0c922a45424b938a9e4fbd541c7f
zj <3642214@163.com>
2014-03-07 10:29:39 +0800

    1.SMTSDVR配置文件在etc目录下，使用etc方式

--- basic/update_config.sh
+++ basic/update_config.sh
@@ -6,7 +6,7 @@ softName=`bash nameToPath.sh $1` || exit $?
 #kill process
 pkill -f $softName/erts.*/bin/beam.smp
 #config
-if [ $1 = "CLT_Master" -o $1 = "CLT_Snode" ]
+if [ $1 = "CLT_Master" -o $1 = "CLT_Snode" -o $1 = "SMTSDVR" ]
 	then
 		bash etcSetConfig.sh $1 || exit $?
 	else
