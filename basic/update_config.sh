8bbd1355d655603a79ff4b3f54f393694c6c082c
zj <3642214@163.com>
2014-03-07 10:33:11 +0800

    1.转为unix格式

--- basic/update_config.sh
+++ basic/update_config.sh
@@ -10,5 +10,5 @@ if [ $1 = "CLT_Master" -o $1 = "CLT_Snode" -o $1 = "SMTSDVR" ]
 	then
 		bash etcSetConfig.sh $1 || exit $?
 	else
-	  bash setConfig.sh $1 || exit $?
+	  bash setConfig.sh $1 || exit $? 
 fi
